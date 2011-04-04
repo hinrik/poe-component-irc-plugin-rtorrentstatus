0eb729c4b2f0324f85089805d463b68cf7ae13e1
Hinrik Örn Sigurðsson <hinrik.sig@gmail.com>
2011-04-04 20:18:21 +0000

    Preserve symlinks in the torrent log path

--- lib/POE/Component/IRC/Plugin/RTorrentStatus.pm
+++ lib/POE/Component/IRC/Plugin/RTorrentStatus.pm
@@ -3,10 +3,10 @@ package POE::Component::IRC::Plugin::RTorrentStatus;
 use strict;
 use warnings FATAL => 'all';
 use Carp qw(croak);
-use Cwd qw(abs_path);
 use DateTime;
 use DateTime::Format::Human::Duration;
 use File::Glob ':glob';
+use File::Spec::Functions 'rel2abs';
 use Format::Human::Bytes;
 use POE::Component::IRC::Plugin qw(PCI_EAT_NONE);
 use POE::Component::IRC::Common qw(NORMAL DARK_GREEN DARK_BLUE ORANGE TEAL BROWN PURPLE MAGENTA);
@@ -24,7 +24,7 @@ sub new {
         croak __PACKAGE__ . ': No channels defined';
     }
 
-    $self->{Torrent_log} = abs_path(bsd_glob($self->{Torrent_log}));
+    $self->{Torrent_log} = rel2abs(bsd_glob($self->{Torrent_log}));
     if (!-e $self->{Torrent_log}) {
         open my $foo, '>', $self->{Torrent_log}
             or die "Can't create $self->{Torrent_log}: $!\n";
